feee38e49e9f66e62aa90785d7f4bfcf116a2571
jkurkure <117855264+jkurkure@users.noreply.github.com>
2024-09-07 21:57:17 +0000

    use localstorage to speed up loading

--- quickdict.py
+++ quickdict.py
@@ -1,38 +1,75 @@
-import sys, os
+import sys, os, platformdirs, pickle
 
 while True:
 	try:
-		from nltk.corpus import brown
+		
 		from rich.progress import track
+		from nltk.corpus import brown
 		from nltk.corpus import wordnet as wn
 		break
 	except ModuleNotFoundError:
 		os.system("pip install nltk rich")
 		continue
 
+data_dir = platformdirs.user_data_dir(appname="quickdict", appauthor="jkurkure")
+if not os.path.exists(data_dir):
+	os.makedirs(data_dir)
+file_path = os.path.join(data_dir, "day-archive.sav")
+
+registry = {}
+
+if os.path.exists(file_path):
+	with open(file_path, "rb") as file:
+		registry = pickle.load(file)
+
+if "dict" not in registry:
+	registry["dict"] = []
+	while True:
+		try:
+			for word in track(brown.words(), description="Loading dictionary"):
+				if word not in registry["dict"]:
+					registry["dict"].append(word)
+			break
+		except LookupError:
+			import nltk
+			nltk.download("brown")
+			continue
+
 # find = lambda w: set([word for word in brown.words() if w == word[:len(w)] ])
-dictry = []
-while True:
-	try:
-		for word in track(brown.words(), description="Loading dictionary"):
-			if word not in dictry:
-				dictry.append(word)
-		break
-	except LookupError:
-		import nltk
-		nltk.download("brown")
-		continue
 
 def find(w, reverse=False):
-	for word in dictry:
+	for word in registry["dict"]:
 		if (w == word[:len(w)] and not reverse) or (w == word[-len(w):] and reverse):
 			yield word
 
 
 if __name__ == "__main__":
+	id = f"results {sys.argv[1]}"
+	if id in registry:
+		f = open('out.tmp', 'w')
+		print(registry[id], file=f)
+		f.close()
+		if os.stat("out.tmp").st_size > 0:  
+			os.system('less out.tmp')
+		else:
+			print("No matches found!")
+		os.remove('out.tmp')
+		exit()
+
 	results = find(sys.argv[1], reverse = sys.argv[-1]=='-r')
 
 	if len(sys.argv) > 2:
+		id += f" {sys.argv[2]}"
+		if id in registry:
+			f = open('out.tmp', 'w')
+			print(registry[id], file=f)
+			f.close()
+			if os.stat("out.tmp").st_size > 0:  
+				os.system('less out.tmp')
+			else:
+				print("No matches found!")
+			os.remove('out.tmp')
+			exit()
 		results = (r for r in results if len(r) >= eval(sys.argv[2]))
 	
 	f = open('out.tmp', 'w')
@@ -56,4 +93,9 @@ if __name__ == "__main__":
 		print("No matches found!")
 		
 	f.close()
+	with open('out.tmp', 'r') as f:
+		registry[id] = f.read()
 	os.remove('out.tmp')
+
+	with open(file_path, "wb") as file:
+		pickle.dump(registry, file)
